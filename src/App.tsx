06b5afb8f7f02cf8606ea49c5e18bf00dd99f6b7
Luisa Carrillo <Luvluvxc@users.noreply.github.com>
2026-05-06 20:57:18 -0600

    react install, tailwind

---
+++ src/App.tsx
@@ -0,0 +1,30 @@
+
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+    <header className="bg-black py-3">
+				<div className="max-w-4xl mx-auto flex justify-between">
+					<h1 className="text-center text-lg font-bold text-white uppercase">
+
+						Contador de Calorias
+
+					</h1>
+				</div>
+			</header>
+			
+			<section className="bg-neutral-800 py-20 px-5">
+
+				<div className="max-w-4xl mx-auto">
+					
+				</div>
+
+
+		</section>
+    </>
+  )
+}
+
+export default App
